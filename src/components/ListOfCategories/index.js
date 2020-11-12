08a32075865f3563218e48021c0966ade419539f
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-11-12 16:38:01 -0500

    Adding useStates and scroll features

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState, Fragment} from 'react'
 import {Category} from '../Category'
 import {List, Item} from './styles'
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState(mockCategories)
+
+  const [showFixed, setShowFixed] = useState (false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200 
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.addEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+)
+  return (
+    <>
+    {renderList()}
+    {showFixed && renderList(true)}
+    </>
   )
 }
